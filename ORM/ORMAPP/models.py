0e6515fb85c8231068f0ea837c285d0c26139bae
Sagar Deepak Thapa <st3541544@gmail.com>
2023-05-25 16:40:23 +0530

    creating a category and post model class in models.py file

--- ORM/ORMAPP/models.py
+++ ORM/ORMAPP/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+class Catogory(models.Model):
+    moviename = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    
+    
+    
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField(max_length=50000)
